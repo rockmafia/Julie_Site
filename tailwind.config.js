98c68bf1575a96038af634aad7c68e623353dd37
Julie <julie@Julies-MacBook-Air.local>
2023-12-07 22:53:28 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'Sepia' : '#F5CCA0' ,//FDA89F
+      'Salmon' : "#FFF1E6"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
