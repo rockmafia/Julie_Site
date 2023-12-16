b1ae940f01a4fcbabb321182a1d549996f6f52ab
rockmafia <rocky_oo@hotmail.com>
2023-12-16 22:22:16 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,7 @@ function App() {
         <div className="grid max-w-screen-xl px-4 py-4 mx-auto lg:gap-8 xl:gap-0 lg:py-8 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7 ">
             <motion.div
-              animate={{ x: 50, scale: 1.2 }}
+              animate={{ x: 50, scale: 1.0 }}
               transition={{ delay: 1 }}
             >
               <h1 className="max-w-2xl mb-2 text-4xl  tracking-tight leading-none md:text-5xl xl:text-7xl dark:text-white pb-8  font-bold mt-6 ">
